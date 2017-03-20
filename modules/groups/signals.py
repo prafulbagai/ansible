fcacc7cd7bad2efddbacc98c48527b743cc16ebc
Praful Bagai <praful.bagai1991@gmail.com>
2017-03-20 15:56:27 +0530

    removed refreshing cache every time a new entry is added.

--- modules/groups/signals.py
+++ modules/groups/signals.py
@@ -7,19 +7,19 @@ from models import GroupCodes, Groups, UnavailableCodes
 # from cache import Cache
 
 
-@receiver(post_save, sender=Groups)
-def update_group_cache(sender, instance, **kwargs):
-    GroupCodes.refresh_cache()
+# @receiver(post_save, sender=Groups)
+# def update_group_cache(sender, instance, **kwargs):
+#     GroupCodes.refresh_cache()
 
-    # created = kwargs.get('created')
-    # # Cache Groups Mapping
-    # if created:  # If new group created, add to cache.
-    #     Cache.set_key(key_type=settings.GROUP_REDIS_KEY, key=instance.name,
-    #                   value=instance.to_json())
-    # else:  # update all the groups again.
-    #     Cache.del_key(key_type=settings.GROUP_REDIS_KEY)
-    #     group = {g.name: g.to_json() for g in Groups.objects.all()}
-    #     Cache.hmset(settings.GROUP_REDIS_KEY, group)
+#     created = kwargs.get('created')
+#     # Cache Groups Mapping
+#     if created:  # If new group created, add to cache.
+#         Cache.set_key(key_type=settings.GROUP_REDIS_KEY, key=instance.name,
+#                       value=instance.to_json())
+#     else:  # update all the groups again.
+#         Cache.del_key(key_type=settings.GROUP_REDIS_KEY)
+#         group = {g.name: g.to_json() for g in Groups.objects.all()}
+#         Cache.hmset(settings.GROUP_REDIS_KEY, group)
 
 
 @receiver(post_save, sender=GroupCodes)
@@ -30,7 +30,7 @@ def update_code_cache(sender, instance, **kwargs):
     UnavailableCodes.objects.filter(master_name=master_name).delete()
 
     # Finally Refreshing cache.
-    GroupCodes.refresh_cache()
+    # GroupCodes.refresh_cache()
 
     # group, master_name = instance.group, instance.master_name.lower()
 
